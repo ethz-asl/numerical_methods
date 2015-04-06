0712ae55fedf9bfd58a3e15c726ee268a6049495
Gabriel Agamennoni <gabriel.agamennoni@mavt.ethz.ch>
2015-04-06 14:08:52 +0200

    added boilerplate code

---
+++ include/numerical-methods/integration/integration-method.h
@@ -0,0 +1,10 @@
+#ifndef NUMERICAL_METHODS_INTEGRATION_INTEGRATION_METHOD_H_
+#define NUMERICAL_METHODS_INTEGRATION_INTEGRATION_METHOD_H_
+
+#include <glog/logging.h>
+
+namespace numerical_methods {
+
+} // namespace numerical_methods
+
+#endif // NUMERICAL_METHODS_INTEGRATION_INTEGRATION_METHOD_H_
--- src/numerical-methods.cc
+++ src/numerical-methods.cc
@@ -1,3 +1,5 @@
+#include "numerical-methods/integration/tanh-sinh-method.h"
+
 namespace numerical_methods {
   
 } // numerical_methods
