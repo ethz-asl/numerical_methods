8e29e7a69744b07e54c928913d3ed56a8d4a5be3
Gabriel Agamennoni <gabriel.agamennoni@mavt.ethz.ch>
2015-04-09 12:40:56 +0200

    tanh-sinh method unit tests passing

--- include/numerical-methods/integration/tanh-sinh-method.h
+++ include/numerical-methods/integration/tanh-sinh-method.h
@@ -15,7 +15,7 @@ class TanhSinhMethod : public IntegrationMethod<Type> {
 public:
   
   explicit TanhSinhMethod(Type error) : 
-      IntegrationMethod<Type>(error), order_(12) {
+      IntegrationMethod<Type>(error), order_(10) {
     initNodes();
   }
   
@@ -38,6 +38,7 @@ public:
     const Type d = (b - a) / 2.0;
     
     std::vector<Type> values;
+    values.reserve(order_);
     
     Type sum = 0.0;
     Type step = 1.0;
@@ -63,13 +64,12 @@ public:
       
       // Estimate error and break if desired error achieved.
       if (estimError(values) <= this->getError()) {
-        return *values.end();
+        return values.back();
       }
       
     }
     
-    LOG(WARNING) << "Maximum order reached. Result may be inaccurate.";
-    return *values.end();
+    return values.back();
     
   }
   
@@ -82,10 +82,10 @@ private:
     Type weight;
     Node(int k, Type h) {
       const Type t = k * h;
-      const Type u = (getPi<Type>() * std::sinh(t)) / 2.0;
+      const Type u = (getPi<Type>() / 2.0) * std::sinh(t);
       const Type v = std::cosh(u);
       point = std::sinh(u) / v;
-      weight = (getPi<Type>() / 2.0) * std::cosh(t) / (v * v);
+      weight = (getPi<Type>() / 2.0) * std::cosh(t) / std::pow(v, 2);
     }
   };
   
@@ -96,7 +96,8 @@ private:
     int n = 20 * std::pow(2, order_) + 1;
     const Type h = 1.0 / std::pow(2, order_);
     for (int k = 0; k < n; ++k) {
-      Node node(k, h);
+      const Node node(k, h);
+      CHECK(!(isUndef(node.point) || isUndef(node.weight)));
       nodes_.push_back(node);
       if (std::abs(node.point - 1.0) <= std::pow(this->getError(), 2)) {
         break;
@@ -106,19 +107,20 @@ private:
   
   // Estimate error from previous values.
   Type estimError(const std::vector<Type>& values) const {
-  	const int i = values.size() - 1;
-  	if (i <= 0) {
-  	  return 1.0;
-  	}
-  	const Type a = std::log10(std::abs(values[i + 1] - values[i]));
-  	const Type b = std::log10(std::abs(values[i + 1] - values[i - 1]));
-  	if (isUndef<Type>(a) || isUndef<Type>(b)) {
-  	  return this->getError();
-  	}
-  	Type digits = std::max<Type>(a * a / b, 2.0 * a);
-  	digits = std::min<Type>(std::max<Type>(digits, 
-            std::log10(getEps<Type>())), 0.0);
-  	return std::pow(0.1, - static_cast<int>(std::round(digits)));
+    int i = values.size() - 1;
+    if (i <= 0) {
+      return 1.0;
+    }
+    --i;
+    const Type a = std::log10(std::abs(values[i + 1] - values[i]));
+    const Type b = std::log10(std::abs(values[i + 1] - values[i - 1]));
+    if (isUndef<Type>(a) || isUndef<Type>(b)) {
+      return this->getError();
+    }
+    Type digits = std::max<Type>(std::pow(a, 2) / b, 2.0 * a);
+    digits = std::min<Type>(std::max<Type>(digits, 
+        std::log10(getEps<Type>())), 0.0);
+    return std::pow(Type(0.1), - static_cast<int>(std::round(digits)));
   }
   
 }; // TanhSinhMethod
